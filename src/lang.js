7dab9abdc7f7f2c9d7e24b0a39905444e7eb760a
fiahfy <d.fiahfy@gmail.com>
2015-02-02 00:56:10 +0900

    update

--- src/lang.js
+++ src/lang.js
@@ -7,20 +7,12 @@
 
 
 export class Object {
-    constructor() {
-    }
-    static get VERSION() {
-        return '1.0.0';
-    }
-    abstractMethod() {
-        throw new Error('Implement abstract method.');
-    }
+  constructor() {
+  }
+  static get VERSION() {
+    return '1.0.0';
+  }
+  abstractMethod() {
+    throw new Error('Implement abstract method.');
+  }
 }
-//var fact = (
-//    (f = n => n>1 ? n*f(n-1) : 1) => f
-//)();
-//var fact1 = (
-//    (f1 = (n) => { return n>1 ? n*f1(n-1) : 1; } ) => f1
-//)();
-//var a = ((b = () => { return 2; }) => b)();
-//console.log(fact1(5));
